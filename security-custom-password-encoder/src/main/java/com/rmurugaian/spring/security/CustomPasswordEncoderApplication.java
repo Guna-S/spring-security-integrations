98ec7c3dbca0de28c7f3c27a0d69f2e3ce11ceda
Ramachandran Murugaian <ramachandran.bitemii@gmail.com>
2019-05-21 09:34:04 +0530

    cache control and xss and https

--- security-custom-password-encoder/src/main/java/com/rmurugaian/spring/security/CustomPasswordEncoderApplication.java
+++ security-custom-password-encoder/src/main/java/com/rmurugaian/spring/security/CustomPasswordEncoderApplication.java
@@ -3,14 +3,10 @@ package com.rmurugaian.spring.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author rmurugaian 2019-05-13
- */
 @SpringBootApplication
 public class CustomPasswordEncoderApplication {
 
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(CustomPasswordEncoderApplication.class, args);
     }
-
 }
